da852ced2c1441b4b7e28bc2683a067c1bc1c09b
mafengyan <18624443327@163.com>
2021-06-29 16:48:55 +0800

    增加演示地址

---
+++ docs/api.js
@@ -0,0 +1,6 @@
+(function () {
+  window.api = {
+    epgms: "/epgms",
+    others: "/other",
+  };
+})();
---
+++ docs/assets/index.b322e2d9.js
@@ -0,0 +1,1 @@
+import{o as e,c as r}from"./vendor.08da2969.js";const t={};t.render=function(t,n,o,a,c,d){return e(),r("h1",null,"template/create")};export default t;
