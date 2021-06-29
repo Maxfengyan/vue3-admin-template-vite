c7d21b1067f772fbe0487d584227dce8b66487d4
mafengyan <18624443327@163.com>
2021-06-29 17:15:38 +0800

    test

--- docs/api.js
+++ docs/api.js
@@ -1,6 +1,8 @@
 (function () {
   window.api = {
-    epgms: "/epgms",
-    others: "/other",
+    epgms:
+      "http://10.10.8.14:9999/mock/5f23d33facd7311a719430ed/epgms",
+    others:
+      "http://10.10.8.14:9999/mock/5f23d3f0acd7311a719430f2/boss",
   };
 })();
--- vite.config.js
+++ vite.config.js
@@ -44,4 +44,7 @@ export default defineConfig({
       },
     }, */
   },
+  build: {
+    outDir: "docs",
+  },
 });
